28f484bd8aebff0f71cb99fe6bf213bef17809b3
glinren <glinren.kre@gmail.com>
2023-06-05 16:03:55 +0200

    added startup message

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main{
     
     public static void main(String[] args){
 	Addressbook book = new Addressbook();
+	System.out.println("Hello! It works, you can compile and execute the project.");
 	
     }
 }
